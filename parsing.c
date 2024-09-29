12a450cec1d07ea42a156a4cfd5b4b68b3e6a8bb
Aaron Padilla <aapadill@student.hive.fi>
2024-09-29 12:56:21 +0300

    small norm fixed

--- parsing.c
+++ parsing.c
@@ -80,10 +80,10 @@ t_cell	**validate_file(char **argv, int *x, int *y)
 {
 	int		fd;
 	char	*line;
-	int		file_name_size;
+	int		file_n_size;
 
-	file_name_size = ft_strlen(argv[1]);
-	if (file_name_size < 5 || ft_strncmp(argv[1] + file_name_size - 4, ".fdf", 4))
+	file_n_size = ft_strlen(argv[1]);
+	if (file_n_size < 5 || ft_strncmp(argv[1] + file_n_size - 4, ".fdf", 4))
 		ft_perror("Invalid file extension", 0);
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
