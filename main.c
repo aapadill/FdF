1492f78a6dd6b1a6652c1deaf55282e6c767ae85
Aaron Padilla <aapadill@student.hive.fi>
2024-09-29 17:19:16 +0300

    huge number fixed

--- main.c
+++ main.c
@@ -30,6 +30,47 @@ static void	init_hook_params(t_hook_params *hook_params, t_map *map)
 	hook_params->map = map;
 }
 
+static	void	find_range(t_map *map, float *range, float *min_val)
+{
+	float	max_val;
+
+	*min_val = map->info.min_x;
+	max_val = map->info.max_x;
+	if (map->info.min_y < *min_val)
+		*min_val = map->info.min_y;
+	if (map->info.min_z < *min_val)
+		*min_val = map->info.min_z;
+	if (map->info.max_y > max_val)
+		max_val = map->info.max_y;
+	if (map->info.max_z > max_val)
+		max_val = map->info.max_z;
+	*range = max_val - *min_val;
+	if (!*range)
+		*range = 1;
+}
+
+static	void	normalize_map(t_map *map)
+{
+	float	range;
+	float	min_val;
+	int		i;
+	int		j;
+
+	compute_center(map);
+	find_range(map, &range, &min_val);
+	i = -1;
+	while (++i < map->y)
+	{
+		j = -1;
+		while (++j < map->x)
+		{
+			map->cells[i][j].x = (map->cells[i][j].x - min_val) / range;
+			map->cells[i][j].y = (map->cells[i][j].y - min_val) / range;
+			map->cells[i][j].z = (map->cells[i][j].z - min_val) / range;
+		}
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	t_map			map;
@@ -41,6 +82,7 @@ int	main(int argc, char **argv)
 	map.cells = validate_file(argv, &map.x, &map.y);
 	fill_cells(&map, argv);
 	ft_bzero(&hook_params, sizeof(hook_params));
+	normalize_map(&map);
 	init_hook_params(&hook_params, &map);
 	display(hook_params.mlx, &map, hook_params.mlx_img, hook_params.centered);
 	mlx_key_hook(hook_params.mlx, &keyhook, &hook_params);
